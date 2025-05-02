0a4ba981e5e6ba55c955d2fbb84fad7957e81be0
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-02 20:14:32 +0900

    feat: 숫자야구

--- week3_assignment/src/App.jsx
+++ week3_assignment/src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import BaseballGame from "./components/BaseballGame";
+import Github from "./components/GitHub";
 
-function App() {
+export default function App() {
+  const [tab, setTab] = useState("github");
   return (
-    <div className="min-h-screen">
-      <Header tab="github" setTab={() => {}} />
+    <div className="min-h-screen bg-gray-50">
+      <Header tab={tab} setTab={setTab} />
+      <main className="pt-32">
+        {tab === "github" && <Github />}
+        {tab === "baseball" && <BaseballGame />}
+      </main>
     </div>
   );
 }
-
-export default App;
--- week3_assignment/src/components/GitHub.jsx
+++ week3_assignment/src/components/GitHub.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const GitHub = () => {
-  return <div></div>;
-};
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-export default GitHub;
+export default GitHub
