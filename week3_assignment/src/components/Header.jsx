086338e2b2b124c6d1aed0db22af872d31295492
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-02 21:34:54 +0900

    refactor: edit the name

--- week3_assignment/src/components/Header.jsx
+++ week3_assignment/src/components/Header.jsx
@@ -1,13 +1,16 @@
 export default function Header({ tab, setTab }) {
   return (
     <header className="fixed top-0 left-0 w-full flex flex-col items-center justify-center py-6 bg-gray-100 shadow z-10">
-      <h1 className="text-2xl font-bold mb-4">⚾️숫자 야구 게임⚾️</h1>
+      <h1 className="text-2xl font-bold mb-4">
+        ⚾️숫자 야구 || 깃허브 검색 🙈
+      </h1>
       <div className="flex gap-4">
         <button
           className={`px-4 py-2 rounded transition
-            ${tab === "github"
-              ? "bg-blue-500 text-white hover:bg-gray-400"
-              : "bg-white border text-blue-500 hover:bg-gray-200"
+            ${
+              tab === "github"
+                ? "bg-blue-500 text-white hover:bg-gray-400"
+                : "bg-white border text-blue-500 hover:bg-gray-200"
             }`}
           onClick={() => setTab("github")}
         >
@@ -15,9 +18,10 @@ export default function Header({ tab, setTab }) {
         </button>
         <button
           className={`px-4 py-2 rounded transition
-            ${tab === "baseball"
-              ? "bg-blue-500 text-white hover:bg-gray-400"
-              : "bg-white border text-blue-500 hover:bg-gray-200"
+            ${
+              tab === "baseball"
+                ? "bg-blue-500 text-white hover:bg-gray-400"
+                : "bg-white border text-blue-500 hover:bg-gray-200"
             }`}
           onClick={() => setTab("baseball")}
         >
